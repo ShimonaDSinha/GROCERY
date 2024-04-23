3159920ed322fe3554d3269b547cbca350c43dbb
ShimonaDSinha <165553271+ShimonaDSinha@users.noreply.github.com>
2024-04-23 20:30:09 +0530

    main.c

--- case_study/main.c
+++ case_study/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include "grocery_billing1.h"
 
@@ -64,4 +64,65 @@ int main() {
     }
 
     return 0;
+ }*/
+//updated:
+#include <stdio.h>
+#include <stdlib.h>
+#include "grocery_billing1.h"
+#include "login.h" // Include login header
+
+int main() {
+    // Authenticate user
+    int authenticated = authenticate();
+    if (!authenticated) {
+        printf("Authentication failed. Exiting...\n");
+        return 1;
+    }
+
+    Item *items = malloc(INITIAL_CAPACITY * sizeof(Item));
+    int numItems = 0;
+    int capacity = INITIAL_CAPACITY;
+    int choice;
+    Customer customer;
+
+    // Getting customer details
+    printf("Enter customer name: ");
+    scanf("%s", customer.name);
+
+    printf("Enter customer address: ");
+    scanf(" %[^\n]", customer.address);
+
+    printf("Enter discount percentage: ");
+    scanf("%f", &customer.discount);
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Add item\n");
+        printf("2. Generate bill\n");
+        printf("3. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                add_item(&items, &numItems, &capacity);
+                break;
+            case 2:
+                generate_receipt(items, numItems, &customer, customer.discount);
+                break;
+            case 3:
+                printf("Thank you for shopping with us %s!\n",customer.name);
+                free(items);
+                exit(0);
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+    }
+
+    return 0;
 }
+
+
+
+
