be7b002c66973cb3b9a9daa87f7b79684d0feee1
ShimonaDSinha <165553271+ShimonaDSinha@users.noreply.github.com>
2024-04-23 20:28:33 +0530

    grocery_billing1.h

--- case_study/grocery_billing1.h
+++ case_study/grocery_billing1.h
@@ -1,4 +1,4 @@
-#ifndef GROCERY_BILLING1_H
+/*#ifndef GROCERY_BILLING1_H
 #define GROCERY_BILLING1_H
 
 #define MAX_NAME_LENGTH 50
@@ -6,5 +6,33 @@
 void add_item(char ***names, float *prices, int *quantities, int *numItems, int *capacity);
 float calculate_total(float *prices, int *quantities, int numItems);
 void generate_receipt(char **names, float *prices, int *quantities, int numItems, char *customerName, char *customerAddress, float discount);
+
+#endif*/
+
+
+//updated code
+
+#ifndef GROCERY_BILLING1_H
+#define GROCERY_BILLING1_H
+
+#define INITIAL_CAPACITY 10
+#define MAX_NAME_LENGTH 50
+#define MAX_ADDRESS_LENGTH 100
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    char address[MAX_ADDRESS_LENGTH];
+    float discount;
+} Customer;
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    float price;
+    int quantity;
+} Item;
+
+void add_item(Item **items, int *numItems, int *capacity);
+float calculate_total(const Item *items, int numItems);
+void generate_receipt(const Item *items, int numItems, const Customer *customer, float discount);
 
 #endif
